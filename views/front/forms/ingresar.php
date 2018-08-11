c92fdadaaf4fd024049b2d99ccbebce160cbbb1a
DESKTOP-78BIM5K\USUARIO <jhonmarmo16@gmail.com>
2018-08-10 22:47:48 -0500

    Loginy register

---
+++ views/front/forms/ingresar.php
@@ -0,0 +1,16 @@
+<?php include ('../../../views/front/templates/head.php');?>
+<body>
+
+	<form class="form2">
+		<div class="tit"><h1>Ingresar</h1></div><br>
+
+		<div><label>Email:</label><input type="text" name="Email" value=""></div>
+		<div><label>Contraseña:</label><input type="password" name="password" value=""></div>
+		<div><label>Confirmar contraseña:</label><input type="password" name="con_pass" value=""><div>
+
+		<div class="reg"><input type="submit" name="registrarse"></div>
+		<div class="ing"><input type="reset" name="iniciar"></div>
+	</form>	
+</body>
+
+<?php include ('../../../views/front/templates/foot.php');?>
